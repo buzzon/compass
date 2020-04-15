f3f8582e26669ce8070c6c6aaf2eeddcdc7128f3
buzzon <buzzondev@gmail.com>
2020-04-15 08:55:20 +0500

    fixed minor bugs with thymeleaf

--- src/main/java/ru/naumen/compass/CompassApplication.java
+++ src/main/java/ru/naumen/compass/CompassApplication.java
@@ -2,9 +2,17 @@ package ru.naumen.compass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CompassApplication {
+public class CompassApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CompassApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CompassApplication.class, args);
 	}
